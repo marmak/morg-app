1a894f96fbb788b5c2365d12b158403296e0ca8e
Martti Mäkinen <marttim9@gmail.com>
2024-04-05 19:59:24 +0300

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
 import { AnkiComponent } from './anki/anki.component';
-
+import { MatMenuModule } from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatMenuModule, RouterModule, AnkiComponent, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
